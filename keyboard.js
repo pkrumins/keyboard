719aaf820c6b3f4e97b48cac30cef75715d69918
Peteris Krumins <peteris.krumins@gmail.com>
2013-03-25 13:41:39 +0200

    turn keyboard into a reusable, browserifyable module

--- keyboard.js
+++ keyboard.js
@@ -12,181 +12,94 @@
 'Shift', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', '<', '>', '?', 'Shift'
 */
 
-var keys = {
-    'shift-off' : {
-        '`' : '~', 
-        '1' : '!', 
-        '2' : '@', 
-        '3' : '#', 
-        '4' : '$', 
-        '5' : '%', 
-        '6' : '^', 
-        '7' : '&', 
-        '8' : '*', 
-        '9' : '(', 
-        '0' : ')', 
-        '-' : '_', 
-        '=' : '+', 
+var hyperglue = require('hyperglue');
+var EventEmitter = require('events').EventEmitter;
+var fs = require('fs');
 
-        'q' : 'Q', 
-        'w' : 'W', 
-        'e' : 'E', 
-        'r' : 'R', 
-        't' : 'T', 
-        'y' : 'Y', 
-        'u' : 'U', 
-        'i' : 'I', 
-        'o' : 'O', 
-        'p' : 'P', 
-        '[' : '{', 
-        ']' : '}', 
+var html = fs.readFileSync(__dirname + '/keyboard.html');
+var css = fs.readFileSync(__dirname + '/keyboard.css');
 
-        'CAPS' : 'CAPS', 
-        'a' : 'A', 
-        's' : 'S', 
-        'd' : 'D', 
-        'f' : 'F', 
-        'g' : 'G', 
-        'h' : 'H', 
-        'j' : 'J', 
-        'k' : 'K', 
-        'l' : 'L', 
-        ';' : ':', 
-        '\'' : '"', 
+var keys = require('./keys').keys;
+var keyCodes = require('./keys').keyCodes;
 
-        'Shift' : 'Shift', 
-        'z' : 'Z', 
-        'x' : 'X', 
-        'c' : 'C', 
-        'v' : 'V', 
-        'b' : 'B', 
-        'n' : 'N', 
-        'm' : 'M', 
-        ',' : '<', 
-        '.' : '>', 
-        '/' : '?', 
-    },
-    'shift-on' : {
-        '~' : '`', 
-        '!' : '1',
-        '@' : '2',
-        '#' : '3',
-        '$' : '4',
-        '%' : '5',
-        '^' : '6',
-        '&' : '7',
-        '*' : '8',
-        '(' : '9',
-        ')' : '0',
-        '_' : '-',
-        '+' : '=',
+var insertedCss = false;
 
-        'Q' : 'q',
-        'W' : 'w',
-        'E' : 'e',
-        'R' : 'r',
-        'T' : 't',
-        'Y' : 'y',
-        'U' : 'u',
-        'I' : 'i',
-        'O' : 'o',
-        'P' : 'p',
-        '{' : '[',
-        '}' : ']',
+module.exports = Keyboard;
 
-        'CAPS' : 'CAPS',
-        'A' : 'a',
-        'S' : 's',
-        'D' : 'd',
-        'F' : 'f',
-        'G' : 'g',
-        'H' : 'h',
-        'J' : 'j',
-        'K' : 'k',
-        'L' : 'l',
-        ':' : ';',
-        '"' : '\'',
+function Keyboard () {
+    if (!(this instanceof Keyboard)) return new Keyboard();
+    EventEmitter.call(this);
+    var self = this;
 
-        'Shift' : 'Shift',
-        'Z' : 'z',
-        'X' : 'x',
-        'C' : 'c',
-        'V' : 'v',
-        'B' : 'b',
-        'N' : 'n',
-        'M' : 'm',
-        '<' : ',',
-        '>' : '.',
-        '?' : '/',
+    var shift = 'off';
+    var caps = 'off';
+
+    if (!insertedCss) {
+        var style = document.createElement('style');
+        style.append(document.createTextNode(css));
+
+        if (document.head.childNodes.length) {
+            document.head.insertBefore(style, document.head.childNodes[0]);
+        }
+        else {
+            document.head.appendChild(style);
+        }
+        insertedCss = true;
     }
-};
 
-var keyCodes = {
-    'Ctrl' : 17,
-    'Alt' : 18,
-    'WinKey' : 21,
-    'OptKey' : 93,
-    'Space' : 32,
-    'ESC' : 27,
-    'F1' : 112,
-    'F2' : 113,
-    'F3' : 114,
-    'F4' : 115,
-    'F5' : 116,
-    'F6' : 117,
-    'F7' : 118,
-    'F8' : 119,
-    'F9' : 120,
-    'F10' : 121,
-    'F11' : 122,
-    'F12' : 123,
-    'Backsp' : 8,
-    'TAB' : 9,
-    'Enter' : 13,
-
-}
-
-var shift = 'off';
-var caps = 'off';
-
-$(function () {
     function changeKeyCase (shiftOrCaps) {
+        /*
         $('#keyboard .row').each(function () {
             $('.key', this).each(function () {
                 $(this).text(keys['shift-' + shiftOrCaps][$(this).text()]);
             });
         });
+        */
     }
+
+    var root = hyperglue(html);
 
     function printKey (keyCode, val) {
-        $('#output').append(val + " (" + keyCode + ") ");
+        //$('#output').append(val + " (" + keyCode + ") ");
     }
 
-    $('#keyboard .shift').click(function () {
-        changeKeyCase(shift);
-        shift = shift == 'off' ? 'on' : 'off';
-    });
+    root.querySelectorAll('.shift').forEach(function (key)) {
+        key.addEventListener('click', function (ev) {
+            changeKeyCase(shift);
+            shift = shift == 'off' ? 'on' : 'off';
+        });
+    }
 
-    $('#keyboard .caps').click(function () {
+    root.querySelector('.caps').addEventListener('click'(function (key)) {
         changeKeyCase(caps);
         caps = caps == 'off' ? 'on' : 'off';
     });
 
-    $('.key').click(function () {
-        var key = $(this).text();
-        if (key == 'Shift' || key == 'Caps') {
-            return;
-        }
-        if (keyCodes[key]) {
-            printKey(keyCodes[key], key);
-        }
-        else {
-            printKey(key.charCodeAt(0), key);
-        }
-        if (shift == 'on') {
-            changeKeyCase(shift);
-            shift = 'off';
-        }
+    root.querySelectorAll('.key').forEach(function (key)) {
+        key.addEventListener('click', function (ev) {
+            //var key = $(this).text();
+            if (key == 'Shift' || key == 'Caps') {
+                return;
+            }
+            if (keyCodes[key]) {
+                this.emit('key', keyCodes[key], key);
+                //printKey(keyCodes[key], key);
+            }
+            else {
+                //printKey(key.charCodeAt(0), key);
+                this.emit('key', key.charCodeAt(0), key);
+            }
+            if (shift == 'on') {
+                changeKeyCase(shift);
+                shift = 'off';
+            }
+        });
     });
-});
 
+    function appendTo (target) {
+        if (typeof target === 'string') {
+            target = document.querySelector(target);
+        }
+        target.appendChild(this.element);
+    }
+}
